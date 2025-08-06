55ef7d2b01ff42743b5fb44a63ce8bdbc41f9e1c
FireFistisDead <yashco.ltd@gmail.com>
2025-08-06 22:40:24 +0530

    using class

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Login from './login.jsx'
-
+import Register_Class from './login_using_class.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    <Login />
+    {/* <Login /> */}
+    <Register_Class />
   </StrictMode>,
 )
